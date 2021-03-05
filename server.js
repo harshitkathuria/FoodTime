0a0a2564019f0e041febe3d63f39c0254ccc42ce
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-05 16:28:25 +0530

    Connected MONGODB to the application

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
 
 const app = express();
+
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB = process.env.DB.replace("<PASSWORD>", DB_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to DB"));
 
 const port = process.env.PORT || 3000;
 
