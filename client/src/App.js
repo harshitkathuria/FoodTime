05214abe36375bacb53bbac5cafc4ad945361b0c
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-07 11:23:31 +0530

    Initialized react app and setup concurrently script

---
+++ client/src/App.js
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">FoodTime</div>;
+}
+
+export default App;
--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongoose
   })
   .then(() => console.log("Connected to DB"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Listening from port ${port}`);
