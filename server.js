31d05e73c8771023dc3e72090266f2641635c8d4
harshitkathuria <harshitkathuria14@gmail.com>
2021-03-14 16:02:57 +0530

    Prepare for deployment

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const morgan = require("morgan");
 const dotenv = require("dotenv");
@@ -9,16 +10,7 @@ const restaurantRoute = require("./routes/restaurantRoute");
 const orderRoute = require("./routes/orderRoute");
 
 dotenv.config();
-
 const app = express();
-
-app.use(express.json());
-app.use(morgan("dev"));
-
-app.use("/api/auth", authRoute);
-app.use("/api/users", userRoute);
-app.use("/api/res", restaurantRoute);
-app.use("/api/order", orderRoute);
 
 const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB = process.env.DB.replace("<PASSWORD>", DB_PASSWORD);
@@ -31,6 +23,22 @@ mongoose
     useUnifiedTopology: true
   })
   .then(() => console.log("Connected to DB"));
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/res", restaurantRoute);
+app.use("/api/order", orderRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
